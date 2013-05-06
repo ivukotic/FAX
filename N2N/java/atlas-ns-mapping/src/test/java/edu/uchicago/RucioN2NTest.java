1a1db6f895529b5a735cab50271024014b95951c
Ilija Vukotic <ivukotic@cern.ch>
2013-05-06 15:51:54 -0500

    change the tests. now in a suite.

--- N2N/java/atlas-ns-mapping/src/test/java/edu/uchicago/RucioN2NTest.java
+++ N2N/java/atlas-ns-mapping/src/test/java/edu/uchicago/RucioN2NTest.java
@@ -1,53 +1,23 @@
 package edu.uchicago;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 import edu.uchicago.xrootd4j.RucioN2N;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
-/**
- * Unit test for simple App.
- */
 public class RucioN2NTest extends TestCase {
 
-	
 	private static Logger log = Logger.getLogger(RucioN2NTest.class);
 	private Properties p=new Properties();
-	private static RucioN2N rucio;
 	
-	public RucioN2NTest(String testName) {
-		super(testName);
-		PropertyConfigurator.configure(RucioN2NTest.class.getClassLoader().getResource("log4j.properties"));
-		p.setProperty("site","MWT2");
-	}
-
-	/**
-	 * @return the suite of tests being tested
-	 */
-	public static Test suite() {
-		return new TestSuite(RucioN2NTest.class);
-	}
-
-
 	public void testRucioInitialization() {
 		log.info("test initialization...");
-		rucio=new RucioN2N(p);
+		PropertyConfigurator.configure(RucioN2NTest.class.getClassLoader().getResource("log4j.properties"));
+		p.setProperty("site","MWT2");
+		RucioN2N rucio=new RucioN2N(p);
+		assert(rucio!=null);
 	}
 	
-	public void testTranslation(){
-		log.info("test translation...");
-		String gLFN1="/atlas/rucio/mc12_8TeV:NTUP_TOP.01213329._025034.root.1";
-		String pfn1=rucio.translate(gLFN1);
-		log.info(pfn1);
-		assertTrue(pfn1!=null);
-		log.info("done. ");
-		String pfn2=rucio.translate(gLFN1);
-		assertTrue(pfn2!=null);
-		log.info("done. ");
-	}
 }
