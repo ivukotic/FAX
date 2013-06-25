c30ea86c3770bced49e42ebe94cd9dc1f91ca6ba
Wahid Bhimji <wbhimji@oldjock.ph.ed.ac.uk>
2013-06-25 16:44:58 +0100

    DPM test changes

--- ROOTIOTests/run-DPM.sh
+++ ROOTIOTests/run-DPM.sh
@@ -8,6 +8,7 @@ export DPM_HOST=gridpp09.ecdf.ed.ac.uk
 export DPNS_HOST=gridpp09.ecdf.ed.ac.uk
 filename="https://gridpp09.ecdf.ed.ac.uk/dpm/ecdf.ed.ac.uk/home/atlas/group.test.hc.NTUP_SMWZ.root"
 filenamehttp="http://gridpp09.ecdf.ed.ac.uk/dpm/ecdf.ed.ac.uk/home/atlas/group.test.hc.NTUP_SMWZ.root"
+filenamexrootd="root://gridpp09.ecdf.ed.ac.uk/dpm/ecdf.ed.ac.uk/home/atlas/group.test.hc.NTUP_SMWZ.root"
 filenamerfio="rfio:////dpm/ecdf.ed.ac.uk/home/atlas/group.test.hc.NTUP_SMWZ.root"
 fi
 if [ "${PANDA_SITE_NAME}" == "ANALY_GLASGOW" ]; then 
@@ -19,11 +20,18 @@ filenamerfio="rfio:////dpm/gla.scotgrid.ac.uk/home/atlas/atlasppsdisk/user.ilija
 fi
 
 treeToUse="physics"
+
+echo "resetting ROOT to default from cvmfs"
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalROOTSetup.sh --skipConfirm 
+
 ln -s $LCG_LOCATION/lib64/libdpm.so libshift.so.2.1
 ln -s $LCG_LOCATION/lib64/liblcgdm.so
 export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
-#root -l -q -b "readint.C++(\"$filenamerfio\",\"$treeToUse\", 100, 30)" >& info.txt
-./readDirect $filenamerfio $treeToUse 100 30 >& info.txt
+
+root -l -q -b "readint.C++(\"$filenamerfio\",\"$treeToUse\", 100, 30)" >& info.txt
+#./readDirect $filenamerfio $treeToUse 100 30 >& info.txt
 
 echo " --------- info.txt ----------"
 cat info.txt
@@ -31,12 +39,7 @@ echo " -----------------------------"
 python uploaderDPM.py "DPM RFIO 100% default cache" "100"
 echo -n "time> DPM-test > test 100,30 Rfio finished "; date
 
-
-echo "resetting ROOT to 5.32 from cvmfs"
 #export LD_LIBRARY_PATH=/cvmfs/atlas.cern.ch/repo/sw/software/i686-slc5-gcc43-opt/17.2.0/LCGCMT/LCGCMT_61c/InstallArea/i686-slc5-gcc43-opt/lib/
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalROOTSetup.sh --skipConfirm 
 source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalGccSetup.sh --gccVersion gcc432_x86_64_slc5
 #source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalPythonSetup.sh --pythonVersion=2.6.5p1-i686-slc5-gcc43
 #echo "=o= checking out ROOT"
@@ -72,7 +75,7 @@ root.exe -l -q -b "readDPMWebDav.C++(\"$filename\",\"$treeToUse\", 100, 0,\"\",\
 echo " --------- info.txt ----------"
 cat info.txt
 echo " -----------------------------"
-#python uploaderDPM.py "DPM WebDav 100 Ev" "100"
+python uploaderDPM.py "DPM WebDav 100 Ev" "100"
 echo -n "time> DPM-test > test 100,0 WebDav finished "; date
 
 echo "=o= DPM WebDav plain http"
