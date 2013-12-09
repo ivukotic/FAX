057d32f58ad331af3d49c3b005de41653695d2c8
Ilija Vukotic <ivukotic@cern.ch>
2013-12-09 11:21:08 -0600

    change to aliased AGIS server.

--- tools/bin/isDSinFAX.py
+++ tools/bin/isDSinFAX.py
@@ -66,7 +66,7 @@ class site:
 sites=[];
 
 try:
-    req = urllib2.Request("http://atlas-agis-api-0.cern.ch/request/service/query/get_se_services/?json&state=ACTIVE&flavour=XROOTD", None)
+    req = urllib2.Request("http://atlas-agis-api.cern.ch/request/service/query/get_se_services/?json&state=ACTIVE&flavour=XROOTD", None)
     opener = urllib2.build_opener()
     f = opener.open(req)
     res=json.load(f)
@@ -85,7 +85,7 @@ except:
 allddms=set()
 
 try:
-    req = urllib2.Request("http://atlas-agis-api-0.cern.ch/request/ddmendpoint/query/list/?json&state=ACTIVE", None)
+    req = urllib2.Request("http://atlas-agis-api.cern.ch/request/ddmendpoint/query/list/?json&state=ACTIVE", None)
     opener = urllib2.build_opener()
     f = opener.open(req)
     res=json.load(f)
