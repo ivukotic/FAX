64c664e8a5f017b7757dee6c1257f7a48c68a9d6
Ilija Vukotic <ivukotic@cern.ch>
2014-01-28 22:45:48 -0600

    try 3

--- ROOTIOTests/run.sh
+++ ROOTIOTests/run.sh
@@ -9,6 +9,7 @@ echo -n "time> begin "; date
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 localSetupROOT --skipConfirm
+asetup 17.6.0,noTest
 
 # compiling releaseFileCache as staticly compiled one does not work on all sites.
 g++ releaseFileCache.cxx -o releaseFileCache
