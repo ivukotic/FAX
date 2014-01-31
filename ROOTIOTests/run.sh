4fd177a6509dab0aaadfc91ae7119a9caf6b6e49
Ilija Vukotic <ivukotic@cern.ch>
2014-01-31 08:31:55 -0600

    try 7

--- ROOTIOTests/run.sh
+++ ROOTIOTests/run.sh
@@ -6,9 +6,9 @@ echo "version 2013/01/28 11:35"
 # this file should be used just to call real test script.
 echo -n "time> begin "; date
 
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-localSetupROOT --skipConfirm
+# export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+# source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+# localSetupROOT --skipConfirm
 
 # compiling releaseFileCache as staticly compiled one does not work on all sites.
 g++ releaseFileCache.cxx -o releaseFileCache
@@ -22,7 +22,7 @@ echo "=============== done"
 
 ls
 
-asetup 17.7.3,noTest
+# asetup 17.7.3,noTest,x86_64-slc5-gcc43-opt
 echo "now should have oracle_cx in it."
 
 #python FDR.py > info.txt;
