fb2dadb216aea645b539862eaaf3e1e62e02d16e
Ilija Vukotic <ivukotic@cern.ch>
2014-03-03 22:06:59 +0100

    re-enabled getting panda related info for HC, WAN, DPM

--- ROOTIOTests/updateHCpanda.sh
+++ ROOTIOTests/updateHCpanda.sh
@@ -1,18 +1,20 @@
 #!/bin/zsh
+
 source /afs/cern.ch/user/i/ivukotic/.zshrc
 
-source /afs/cern.ch/project/gd/LCG-share/current/etc/profile.d/grid_env.sh 
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+
+localSetupEmi
 voms-proxy-init -voms atlas -pwstdin < /afs/cern.ch/user/i/ivukotic/gridlozinka.txt
 
-export AtlasSetup=/afs/cern.ch/atlas/software/releases/17.0.4/AtlasSetup
-alias asetup='source $AtlasSetup/scripts/asetup.sh'
+asetup 17.2.7,single,here,slc5
 
-source $AtlasSetup/scripts/asetup.sh rel_0,noTest
 source /afs/cern.ch/atlas/offline/external/GRID/DA/panda-client/latest/etc/panda/panda_setup.sh
 
-cd /afs/cern.ch/user/i/ivukotic/ROOTIOTests
+cd /afs/cern.ch/user/i/ivukotic/FAX/ROOTIOTests
 
 
 python updatePanda.py
-#python updatePandaWAN.py
-#python updatePandaDPM.py
+python updatePandaWAN.py
+python updatePandaDPM.py
