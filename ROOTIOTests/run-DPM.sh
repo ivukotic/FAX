9500f368d41586e86a7ff395b3885df10e5bcbe0
Fabrizio Furano <furano@lxplus0130.cern.ch>
2014-03-31 14:31:02 +0200

    Added ZCU as dest site

--- ROOTIOTests/run-DPM.sh
+++ ROOTIOTests/run-DPM.sh
@@ -7,13 +7,14 @@ urls=("http://lxfsra04a04.cern.ch/dpm/cern.ch/home/atlas/group.test.hc.NTUP_SMWZ
 "http://gridpp09.ecdf.ed.ac.uk/dpm/ecdf.ed.ac.uk/home/atlas/group.test.hc.NTUP_SMWZ.root" \
 "root://gridpp09.ecdf.ed.ac.uk//dpm/ecdf.ed.ac.uk/home/atlas/group.test.hc.NTUP_SMWZ.root" \
 "http://atlasdisk1.lnf.infn.it/dpm/lnf.infn.it/home/atlas/group.test.hc.NTUP_SMWZ.root" \
-"root://eosatlas.cern.ch//eos/atlas/atlaseosdatadisk/rucio/user/flegger/60/e3/NTUP_SUSYSKIM.01106323._000003.WIGNER.root.1")
+"root://eosatlas.cern.ch//eos/atlas/atlaseosdatadisk/rucio/user/flegger/60/e3/NTUP_SUSYSKIM.01106323._000003.WIGNER.root.1" \
+"http://dpmrc-head.civ.zcu.cz/dpm/zcu.cz/home/dteam/group.test.hc.NTUP_SMWZ.root")
 
 # These are the more friendly dest names
-destnames=("CERN_DPM_TRUNK" "CERN_DPM_TRUNK" "DESY_DCACHETEST" "GRIDPP_DPM_EPELTEST" "GRIDPP_DPM_EPELTEST" "LNF_DPM_EPELTEST" "CERN_EOS")
+destnames=("CERN_DPM_TRUNK" "CERN_DPM_TRUNK" "DESY_DCACHETEST" "GRIDPP_DPM_EPELTEST" "GRIDPP_DPM_EPELTEST" "LNF_DPM_EPELTEST" "CERN_EOS" "ZCU_DPM_RC")
 
 # And this is a human readable word that tells us what we tested
-desttag=("DPMhttp" "DPMxrootd" "DCACHEhttp" "DPMhttp" "DPMxrootd" "DPMhttp" "EOSxrootd")
+desttag=("DPMhttp" "DPMxrootd" "DCACHEhttp" "DPMhttp" "DPMxrootd" "DPMhttp" "EOSxrootd" "DPMhttp")
 
 for i in 0 1 2 3 4 5 6 7; do
  dest=${urls[$i]}
