dda24b97b6d1182f03fe1da0cea5156d97fca69e
Fabrizio Furano <furano@lxplus0017.cern.ch>
2014-06-10 17:43:39 +0200

    Enable Davix in the http tests :-O

--- ROOTIOTests/run-DPMsingle.sh
+++ ROOTIOTests/run-DPMsingle.sh
@@ -11,8 +11,8 @@ export DEST_SITE_NAME=$1
 treeToUse="physics"
 echo "resetting ROOT to default from cvmfs"
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalROOTSetup.sh --skipConfirm 
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh 
+source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalROOTSetup.sh --skipConfirm 5.34.18-davix_p6-x86_64-slc6-gcc48-opt
 
 export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
 
