af287438989a1ae13bd274bb06996196492c7c55
Ilija Vukotic <ivukotic@cern.ch>
2014-06-17 18:42:18 +0200

    tut update

--- Tutorial/ReadingWritingFile/readWriteFile.sh
+++ Tutorial/ReadingWritingFile/readWriteFile.sh
@@ -1,7 +1,7 @@
 make
 
 ./readWrite \
-root://fax.mwt2.org:1094//atlas/rucio/user/ivukotic:group.test.hc.NTUP_SMWZ.root \
+$STORAGEPREFIX/atlas/rucio/user/ivukotic:group.test.hc.NTUP_SMWZ.root \
 /tmp/out_$RANDOM.root \
 physics \
 10 \
