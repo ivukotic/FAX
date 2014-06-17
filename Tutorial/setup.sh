3af3951fb671fc72a6a07732c674fa170e959698
Ilija Vukotic <ivukotic@cern.ch>
2014-06-17 17:04:18 +0200

    tut update

--- Tutorial/setup.sh
+++ Tutorial/setup.sh
@@ -1,4 +1,6 @@
  export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
  source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh --quiet
- localSetupFAX
+
+ localSetupFAX --rootVersion=current-SL6 --xrootdVersion=current-SL6
+
  voms-proxy-init -voms atlas
