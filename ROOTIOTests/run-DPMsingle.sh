39b149ac6bbf5a33b50af8160889e8d17a7a7043
Fabrizio Furano <furano@cern.ch>
2014-06-30 10:21:11 +0200

    Use the most recent official ROOT version, with Davix and Xrootd support
    5.34.18-davix_p7-x86_64-slc6-gcc48-opt

--- ROOTIOTests/run-DPMsingle.sh
+++ ROOTIOTests/run-DPMsingle.sh
@@ -12,7 +12,7 @@ treeToUse="physics"
 echo "resetting ROOT to default from cvmfs"
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh 
-source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalROOTSetup.sh --skipConfirm 5.34.18-davix_p6-x86_64-slc6-gcc48-opt
+source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalROOTSetup.sh --skipConfirm 5.34.18-davix_p7-x86_64-slc6-gcc48-opt
 
 export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
 
