d99bd186960139d8b0c439b06d5881405baca2af
Ilija Vukotic <ivukotic@cern.ch>
2014-07-24 13:51:52 -0500

    fix

--- Tutorial/Derivation/setup.sh
+++ Tutorial/Derivation/setup.sh
@@ -5,9 +5,14 @@
 
  voms-proxy-init -voms atlas
 
-echo "creating input file list"
+echo "creating input file list..."
 fax-get-gLFNs valid2.117050.PowhegPythia_P2011C_ttbar.digit.AOD.e2657_s1933_s1964_r5534/ > inputDataList.txt
 
+echo "seting up athena environment..."
 asetup 19.1.X.Y-VAL,rel_4,AtlasDerivation,here
 
-Reco_tf.py --inputAODFile inputXAOD.pool.root --outputDAODFile output --reductionConf TEST1 TEST2 TEST3 TEST4
+
+inpFile=$(head -n 1 inputDataList.txt)
+echo "Running the train..."
+Reco_tf.py --inputAODFile $inpFile --outputDAODFile output --reductionConf TEST1 TEST2 TEST3 TEST4 
+echo "Done"
