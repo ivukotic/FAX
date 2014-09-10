566ddc6bcb9c9b765091e822a13a2bff2e52930d
Ilija Vukotic <ivukotic@cern.ch>
2014-09-09 21:30:28 -0500

    reverted back to xrootd 3 and now returning non 0 code if file could not have been opened.

--- tools/bin/FAX-listFileContent.C
+++ tools/bin/FAX-listFileContent.C
@@ -1,6 +1,10 @@
 void FAX_listFileContent(string fn){
-	TFile *f = TFile::Open(fn.c_str());
-	if (f)	f->ls();
-    else std::cout<<"File could not be opened."<<std::endl;
+    TFile *f = TFile::Open(fn.c_str());
+    if (f){ 
+        f->ls();
+    } else {
+        std::cout<<"File could not be opened."<<std::endl;
+        exit(1);
+    }
 }
 
