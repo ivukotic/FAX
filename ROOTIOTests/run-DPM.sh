f962df328278510c57f33b5aee5415ca254dea55
Fabrizio Furano <furano@cern.ch>
2015-02-24 16:11:15 +0100

    Add littlexrdhttp with plain http

--- ROOTIOTests/run-DPM.sh
+++ ROOTIOTests/run-DPM.sh
@@ -12,6 +12,7 @@ urls=("https://lxfsra04a04.cern.ch/dpm/cern.ch/home/atlas/group.test.hc.NTUP_SMW
 "https://lxbst2320.cern.ch/eos/ppsscratch/group.test.hc.NTUP_SMWZ.root" \
 "https://dpmrc-head.civ.zcu.cz/dpm/zcu.cz/home/dteam/group.test.hc.NTUP_SMWZ.root" \
 "https://littlexrdhttp.cern.ch:1094/dynafeds_demo/HCfunc/group.test.hc.NTUP_SMWZ.root" \
+"http://littlexrdhttp.cern.ch:1094/dynafeds_demo/HCfunc/group.test.hc.NTUP_SMWZ.root" \
 "root://littlexrdhttp.cern.ch:1094//dynafeds_demo/HCfunc/group.test.hc.NTUP_SMWZ.root" \
 "https://dc2-grid-23.brunel.ac.uk/dpm/brunel.ac.uk/home/atlas/group.test.hc.NTUP_SMWZ.root" \
 "root://dc2-grid-23.brunel.ac.uk//atlas/group.test.hc.NTUP_SMWZ.root"
@@ -22,12 +23,12 @@ urls=("https://lxfsra04a04.cern.ch/dpm/cern.ch/home/atlas/group.test.hc.NTUP_SMW
 
 
 # These are the more friendly dest names
-destnames=("CERN_DPM_TRUNK" "CERN_DPM_TRUNK" "DESY_DCACHETEST" "GRIDPP_DPM_EPELTEST" "GRIDPP_DPM_EPELTEST" "LNF_DPM_EPELTEST" "LNF_DPM_EPELTEST" "CERN_EOS" "CERN_EOS" "ZCU_DPM_RC" "CERN_LITTLEXRDHTTP" "CERN_LITTLEXRDHTTP" "BRUNEL_EPELTEST" "BRUNEL_EPELTEST" "LAPP_EPELTEST" "LAPP_EPELTEST" "DYNAFED_DESY")
+destnames=("CERN_DPM_TRUNK" "CERN_DPM_TRUNK" "DESY_DCACHETEST" "GRIDPP_DPM_EPELTEST" "GRIDPP_DPM_EPELTEST" "LNF_DPM_EPELTEST" "LNF_DPM_EPELTEST" "CERN_EOS" "CERN_EOS" "ZCU_DPM_RC" "CERN_LITTLEXRDHTTP" "CERN_LITTLEXRDHTTP" "CERN_LITTLEXRDHTTP" "BRUNEL_EPELTEST" "BRUNEL_EPELTEST" "LAPP_EPELTEST" "LAPP_EPELTEST" "DYNAFED_DESY")
 
 # And this is a human readable word that tells us what we tested
-desttag=("DPMhttps" "DPMxrootd" "DCACHEhttp" "DPMhttps" "DPMxrootd" "DPMhttps" "DPMxrootd" "EOSxrootd" "EOShttps" "DPMhttps" "LittleHTTPS" "Littlexrootd" "DPMhttps" "DPMxrootd" "DPMhttps" "DPMxrootd" "DYNAFEDhttps")
+desttag=("DPMhttps" "DPMxrootd" "DCACHEhttp" "DPMhttps" "DPMxrootd" "DPMhttps" "DPMxrootd" "EOSxrootd" "EOShttps" "DPMhttps" "LittleHTTPS" "LittleHTTP" "Littlexrootd" "DPMhttps" "DPMxrootd" "DPMhttps" "DPMxrootd" "DYNAFEDhttps")
 
-for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16; do
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17; do
  dest=${urls[$i]}
  dtag=${desttag[$i]}
  dsite=${destnames[$i]}
